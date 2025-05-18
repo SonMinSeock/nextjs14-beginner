7c99d2b072856cee96b13d2b48125a1cd9e2aa40
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-05-18 17:58:32 +0900

    feat: Parallel Requests

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -1,14 +1,15 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Home',
 };
 
-const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
 const getMovies = async () => {
-  new Promise((resolve) => setTimeout(resolve, 10000));
-  const response = await fetch(URL);
+  // new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(API_URL);
   const json = await response.json();
   return json;
 };
@@ -16,5 +17,13 @@ const getMovies = async () => {
 export default async function Home() {
   const movies = await getMovies();
 
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
---
+++ app/(movies)/movies/[id]/loading.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Loading() {
+  return <h2>Loading a movie :id</h2>;
+}
+
+export default Loading;
