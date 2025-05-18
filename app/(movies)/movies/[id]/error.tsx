a8058ecef72ee8c2ed7dbde12f1dd27dc2738b75
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-05-18 20:45:51 +0900

    feat: Error Handling

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react';
+
+function Error() {
+  return <h1>Something Broken!</h1>;
+}
+
+export default Error;
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,9 +1,10 @@
 import { API_URL } from '../app/(Home)/page';
 
 async function getVideos(id: string) {
-  //   new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
+  new Promise((resolve) => setTimeout(resolve, 5000));
+  throw new Error('something broke...');
+  // const response = await fetch(`${API_URL}/${id}/videos`);
+  // return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
