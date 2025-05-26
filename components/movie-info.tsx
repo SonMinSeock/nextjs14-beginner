1a20bcc56e9d86310efa00e70a5a442e2b1d5b15
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-05-26 11:50:50 +0900

    feat: Movie Trailers

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from '../app/(Home)/page';
+import styles from '../styles/movie-info.module.css';
 
 async function getMovie(id: string) {
   //   new Promise((resolve) => setTimeout(resolve, 3000));
@@ -9,5 +10,17 @@ async function getMovie(id: string) {
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
 
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={styles.container}>
+      <img className={styles.poster} src={movie.poster_path} />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={'_blank'} rel='noreferrer'>
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
