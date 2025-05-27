673ebf5a4d3c37d512a57abb14a3be2c90b8d842
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-05-27 19:40:24 +0900

    feat: Dynamic Metadata

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,7 +1,7 @@
 import { API_URL } from '../app/(Home)/page';
 import styles from '../styles/movie-info.module.css';
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
   //   new Promise((resolve) => setTimeout(resolve, 3000));
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
